90e548e804f8e409c3e410f33a712464562747fb
Elena <eispravnikova@gpm-rtv.ru>
2022-11-01 20:42:14 +0300

    fix

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 
+//отключает стандартное предупреждение vue в консоли браузера о работе в режиме разработки.
 Vue.config.productionTip = false;
 
 new Vue({
